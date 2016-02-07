f8a5b5e8ed482784321b2cd4832276e69e4d7ad3
Daniel King <damaki.gh@gmail.com>
2016-02-07 13:35:38 -0400

    Rename to Turtyl

--- src/turtlegraphicsscene.cpp
+++ src/turtlegraphicsscene.cpp
@@ -2,9 +2,9 @@
  * Copyright (c) 2016 Daniel King
  * Turtle graphics with Lua
  *
- * This file is part of Tartaruga.
+ * This file is part of Turtyl.
  *
- * Tartaruga is free software: you can redistribute it and/or modify
+ * Turtyl is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
